fefc14aa924434b78bf18948cae967973df43e5d
yonasfissha <yonasfessha@gmail.com>
2020-10-17 11:13:52 +0300

    inital commit of my component without commit

---
+++ src/components/header/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './styles.scss'
+import logo from '../../assets/logo.png'
+const Header = (props) => {
+    return (
+        <header>
+            <div className="wrap">
+            <div className="logo">
+                <img src={logo} alt="logo"/>
+            </div>
+            </div>
+        </header>
+    )
+}
+
+export default Header
